ca4a721859c839253a36c6b8ce154019beae7d5a
ahoken50 <micross50@gmail.com>
2025-11-28 12:27:57 -0500

    feat: add Header component with Val-d'Or logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,20 +7,11 @@ export const Header: React.FC = () => {
       <div className="flex flex-col items-start">
         {/* Logo composition */}
         <div className="flex items-center gap-2">
-          {/* Stylized Logo Placeholder */}
-          <div className="relative w-12 h-12 flex items-center justify-center">
-            {/* Artistic representation of the V logo in the screenshot */}
-            <div className="absolute inset-0 bg-black/80 skew-x-12 w-4 h-12 left-2 rounded-sm"></div>
-            <div className="absolute inset-0 bg-red-500/90 -skew-x-12 w-4 h-6 left-0 top-6 rounded-sm"></div>
-            <div className="absolute inset-0 bg-yellow-400/90 skew-x-12 w-4 h-6 left-6 top-6 rounded-sm"></div>
-            <div className="absolute inset-0 bg-green-600/90 -skew-x-12 w-2 h-8 left-3 top-8 rounded-sm z-10"></div>
-          </div>
-
-          <div className="flex flex-col">
-            <span className="text-3xl font-bold text-black leading-none tracking-tight">
-              Val-d'Or
-            </span>
-          </div>
+          <img
+            src="/src/assets/logo-val-dor.png"
+            alt="Ville de Val-d'Or"
+            className="h-16 w-auto object-contain"
+          />
         </div>
       </div>
     </div>
