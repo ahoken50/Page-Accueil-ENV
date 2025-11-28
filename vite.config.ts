08597d3dd4199113beace015feaaadfbfc861f93
ahoken50 <micross50@gmail.com>
2025-11-28 11:29:50 -0500

    fix: Set Vite base path to `/Page-Accueil-ENV/`.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Update this to '/repo-name/' if deploying to https://<user>.github.io/<repo-name>/
+  base: '/Page-Accueil-ENV/', // Update this to '/repo-name/' if deploying to https://<user>.github.io/<repo-name>/
 })
